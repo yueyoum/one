1857ae51869e28d131dbaa9b447f8823172d7ee1
yueyoum <yueyoum@gmail.com>
2015-08-19 19:18:02 +0800

    get images

--- main/views.py
+++ main/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.http import JsonResponse
+from django.db import connection
+
+from main.models import Image
+
+def get_images(request):
+    with connection.cursor() as c:
+        c.execute("select max(id) from {0}".format(Image._meta.db_table))
+        maxid = c.fetchone()[0]
+
+    amount = 50 if maxid > 50 else maxid
+
+    data = []
+    while not data:
+        data = do_get(maxid, amount)
+
+    return JsonResponse({'images': data})
+
+
+def do_get(maxid, amount):
+    ids = random.sample(xrange(maxid), amount)
+
+    data = []
+    for i in Image.objects.filter(id__in=ids):
+        image = {
+            'url': i.link_url(),
+            'w': i.width,
+            'h': i.height,
+        }
+
+        data.append(image)
+
+    random.shuffle(data)
+    return data
