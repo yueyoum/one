377b4e77af8fb21751864f5a0ca56a5a88392a6e
yueyoum <yueyoum@gmail.com>
2015-08-24 10:43:08 +0800

    admin

--- main/admin.py
+++ main/admin.py
@@ -5,11 +5,13 @@ from main.models import Image
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'original_url', 'width', 'height', 'Link',
+        'id', 'original_url', 'width', 'height', 'Img',
         'process_done', 'display',
         'result',
     )
 
-    def Link(self, obj):
-        return '<a href="{0}">{0}</a>'.format(obj.link_url())
-    Link.allow_tags = True
+    list_per_page = 50
+
+    def Img(self, obj):
+        return '<a href="{0}" target="_blank"><img src="{0}" width="200px"/></a>'.format(obj.link_url())
+    Img.allow_tags = True
